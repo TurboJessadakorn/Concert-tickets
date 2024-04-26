b0d85b37ae7408f259c9dfd7de9e34d066a92996
TURBO <jessadakorn.paopati@gmail.com>
2024-04-26 12:38:16 +0700

    Handle error will custom exception

---
+++ backend/src/filters/exception.filter.ts
@@ -0,0 +1,33 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter {
+  private readonly logger: Logger
+  constructor() {
+    this.logger = new Logger
+  }
+  catch(exception: Error, host: ArgumentsHost): any {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+    const statusCode = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
+    const message = exception instanceof HttpException ? exception.message : 'Internal server error';
+
+
+    const devErrorResponse: any = {
+      statusCode,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      errorName: exception?.name,
+      message: exception?.message
+    };
+
+    const returnErrorResponse: any = {
+      statusCode,
+      message
+    };
+    this.logger.log(`request method: ${request.method} request url${request.url}`, JSON.stringify(devErrorResponse));
+    response.status(statusCode).json(returnErrorResponse);
+  }
+}
