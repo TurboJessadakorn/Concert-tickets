fd61f7d9241dde8623a0f380bbcbb1d566dc6ee6
TURBO <jessadakorn.paopati@gmail.com>
2024-04-26 14:54:17 +0700

    Enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { HttpErrorFilter } from './filters/exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpErrorFilter());
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
