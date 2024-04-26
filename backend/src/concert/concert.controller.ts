a02a13444e396cd0a87c62a3cdc12abf5111ec6f
TURBO <jessadakorn.paopati@gmail.com>
2024-04-26 13:16:28 +0700

    Create concert controller and service

--- backend/src/concert/concert.controller.ts
+++ backend/src/concert/concert.controller.ts
@@ -1,4 +1,91 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, Param, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Concert } from './concert.entity';
+import { ConcertService } from './concert.service';
+import { CreateConcertDto } from './concert.dto';
+import { UserGuard } from 'src/guards/user.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { ObjectId } from 'typeorm';
 
 @Controller('concert')
-export class ConcertController {}
+export class ConcertController {
+  constructor(private readonly concertService: ConcertService) { }
+
+  // Create a new concert (for admin)
+  @Post()
+  @UseGuards(AdminGuard)
+  async createConcert(@Body(new ValidationPipe()) concertData: CreateConcertDto): Promise<object> {
+    try {
+      const createdConcert = await this.concertService.createConcert(concertData);
+      return {
+        success: true,
+        message: 'Concert created successfully',
+        data: createdConcert,
+      };
+    } catch (error) {
+      throw new error;
+    }
+  }
+
+  // Delete a concert by ID (for admin)
+  @Delete(':id')
+  @UseGuards(AdminGuard)
+  async deleteConcert(@Param('id') id: number): Promise<object> {
+    try {
+      await this.concertService.deleteConcert(id);
+      return {
+        success: true,
+        message: 'Concert deleted successfully',
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Retrieve all concerts (for admin)
+  @Get()
+  @UseGuards(AdminGuard)
+  async findAllConcerts(): Promise<object> {
+    try {
+      const concerts = await this.concertService.findAllConcerts();
+      return {
+        success: true,
+        message: 'Concerts retrieved successfully',
+        data: concerts,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Reserve a seat for a concert (for user)
+  @Post(':id/reserve/:userId')
+  @UseGuards(UserGuard)
+  async reserveSeatConcert(@Param('id') id: ObjectId, @Param('userId') userId: string): Promise<object> {
+    try {
+      const updatedConcert = await this.concertService.reserveSeat(id, userId);
+      return {
+        success: true,
+        message: 'Seat reserved successfully',
+        data: updatedConcert,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Cancel a seat reservation for a concert (for user)
+  @Post(':id/cancel/:userId')
+  @UseGuards(UserGuard)
+  async cancelSeat(@Param('id') id: ObjectId, @Param('userId') userId: string): Promise<object> {
+    try {
+      const updatedConcert = await this.concertService.cancelSeat(id, userId);
+      return {
+        success: true,
+        message: 'Seat reservation canceled successfully',
+        data: updatedConcert,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
--- backend/src/concert/concert.service.ts
+++ backend/src/concert/concert.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ObjectId, Repository } from 'typeorm';
+import { Concert } from './concert.entity';
 
 @Injectable()
-export class ConcertService {}
+export class ConcertService {
+    constructor(
+        @InjectRepository(Concert)
+        private readonly concertRepository: Repository<Concert>,
+    ) { }
+
+    async createConcert(concertData: Partial<Concert>): Promise<Concert> {
+        const concert = this.concertRepository.create(concertData);
+        return this.concertRepository.save(concert);
+    }
+
+    async deleteConcert(id: number): Promise<void> {
+        await this.concertRepository.delete(id);
+    }
+
+    async findAllConcerts(): Promise<Concert[]> {
+        return this.concertRepository.find();
+    }
+
+    async reserveSeat(concertId: ObjectId, userId: string): Promise<Concert> {
+        const concert = await this.concertRepository.findOne({ where: { _id: concertId } });
+        if (!concert) {
+            throw new NotFoundException('Concert not found');
+        }
+
+        if (concert.reservedSeats >= concert.totalSeats) {
+            throw new BadRequestException('No available seats to reserve');
+        }
+
+        concert.reservedSeats++;
+        concert.reservedBy.push(userId);
+
+        return this.concertRepository.save(concert);
+    }
+
+    async cancelSeat(concertId: ObjectId, userId: string): Promise<void> {
+        const concert = await this.concertRepository.findOne({ where: { _id: concertId } });
+        if (!concert) {
+            throw new NotFoundException("Concert not found");
+        }
+
+        if (!concert.reservedBy.includes(userId)) {
+            throw new BadRequestException('User did not reserve a seat');
+        }
+
+        concert.reservedSeats--;
+        concert.reservedBy = concert.reservedBy.filter(id => id !== userId);
+
+        await this.concertRepository.save(concert);
+    }
+}
